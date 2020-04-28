8b761d0a801544ca55bcb6bc25975cfdc8b1698e
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-04-29 02:37:56 +0700

    .

--- app/src/main/java/com/gzeinnumer/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/daggerpractice/ui/main/MainActivity.java
@@ -2,8 +2,12 @@ package com.gzeinnumer.daggerpractice.ui.main;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.gzeinnumer.daggerpractice.BaseActivity;
@@ -19,5 +23,21 @@ public class MainActivity extends BaseActivity {
 
         Log.d(TAG, "onCreate: created");
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                sessionManager.logOut();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
